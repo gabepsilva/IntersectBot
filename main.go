abb42e2fab98f20ac13867478f100689d48f8cc2
Gabriel da Silva <gabriel@psilva.email>
2020-01-25 02:54:39 -0500

    discovering slack api + .gitignore

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+
 
 func main(){
-	fmt.Print("Initial Commit")
+
+
+	router := gin.New()
+
+	// Simple group: v1
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/hi", commandHi)
+	}
+
+	err := router.Run("0.0.0.0:8000")
+	if err != nil {
+		log.Panicf("%v", err)
+	}
+
+}
+
+func commandHi(c *gin.Context){
+
+	greetings := []string{
+		"Enjoy your special day.",
+		"The day is all yours - have fun!",
+	}
+
+
+	err := c.Request.ParseForm()
+	if err != nil {
+		log.Printf("invalid request from server: %v", err)
+	}
+
+	log.Println(c.Request.FormValue("text"))
+	c.String(200, "%s", greetings[1])
+
 }
