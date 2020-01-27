575ca1ae53b17519ff9454aebe9046034b61ecda
Gabriel da Silva <gabriel@psilva.email>
2020-01-27 14:35:04 -0500

    Saving weather in DB + small refactor

---
+++ appDB/appDB.go
@@ -0,0 +1,51 @@
+package appDB
+
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+type MySQLDB struct   {
+	url string
+	db *sql.DB
+}
+//
+var MySQL * MySQLDB
+
+func Connect(url string){
+
+	MySQL = new(MySQLDB)
+	MySQL.url = url
+
+	db, err := sql.Open("mysql", MySQL.url)
+	if err != nil {
+		log.Panicf("can not connect: %v", err)
+	}
+	MySQL.db = db
+}
+
+func (mysql *MySQLDB) Exec(query string) sql.Result{
+
+	ret, err := mysql.db.Exec(query)
+	if err != nil {
+		log.Panicf("query failed: %v", err)
+	}
+	return ret
+}
+
+func (mysql *MySQLDB) Init(){
+
+	_ = mysql.Exec("CREATE SCHEMA IF NOT EXISTS weather;")
+	_ = mysql.Exec("USE weather")
+
+	_ = mysql.Exec(`CREATE TABLE IF NOT EXISTS weather
+(
+city VARCHAR(24) NOT NULL,
+weather VARCHAR(24) NOT NULL,
+temperature FLOAT NOT NULL,
+check_time TIMESTAMP DEFAULT NOW() null
+);`)
+
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/briandowns/openweathermap v0.13.0
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
